a81dfbe3c80ac475fa044d91fbb8dcc1a05719b0
Aiperi <aip.amankulova@gmail.com>
2020-10-22 18:47:13 +0600

    Add new styles for buttons

--- app/src/main/java/com/example/design/QuestionsActivity.kt
+++ app/src/main/java/com/example/design/QuestionsActivity.kt
@@ -3,6 +3,7 @@ package com.example.design
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_questions.*
+import kotlinx.android.synthetic.main.item_questions.*
 
 class QuestionsActivity : AppCompatActivity() {
 
